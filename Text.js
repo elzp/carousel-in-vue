f9ea727c10f751f20419ea295a96a2c52f038e91
elzp <86740659+elzp@users.noreply.github.com>
2022-10-19 18:29:37 +0200

    Add ability to change logo & text every click

--- Text.js
+++ Text.js
@@ -1,42 +1,52 @@
+const texts = [
+  `
+
+As <span class="text-gray-900">Slack</span> grows rapidly,
+using Stripe helps them scale payments easily—supporting
+everything from getting paid by users around the world to
+enabling ACH payments for corporate customers.
+
+`,
+  `
+With over 700,000 drivers providing nearly one million rides
+per day, <span class="text-gray-900">Lyft </span> uses Stripe
+to power payments at scale.
+<span class="text-gray-900">Lyft </span> also partnered with
+Stripe to build Express Pay, a first-of-its-kind feature that
+allows drivers to cash out whenever they want, instantly.
+`,
+  `
+With Stripe’s reliable and developer-friendly tools,
+<span class="text-gray-900">Glossier</span>’s engineering team
+is freed up to focus on product and customer experiences
+`,
+  `
+<span class="text-gray-900">charity: water</span> optimised
+its mobile and web donation flows with Stripe so it can focus
+on what really matters: bringing clean water to every person
+on the planet. Using Stripe, it also built a new monthly
+giving programme, which makes it even easier for subscribers
+to donate throughout the year.
+`,
+];
+
 export default {
   name: 'Text',
   data() {
-    const texts = [
-      `
-  
-    As <span class="text-gray-900">Slack</span> grows rapidly,
-    using Stripe helps them scale payments easily—supporting
-    everything from getting paid by users around the world to
-    enabling ACH payments for corporate customers.
-  
-  `,
-      `
-    With over 700,000 drivers providing nearly one million rides
-    per day, <span class="text-gray-900">Lyft </span> uses Stripe
-    to power payments at scale.
-    <span class="text-gray-900">Lyft </span> also partnered with
-    Stripe to build Express Pay, a first-of-its-kind feature that
-    allows drivers to cash out whenever they want, instantly.
-   `,
-      `
-  With Stripe’s reliable and developer-friendly tools,
-  <span class="text-gray-900">Glossier</span>’s engineering team
-  is freed up to focus on product and customer experiences
-`,
-      `
-  <span class="text-gray-900">charity: water</span> optimised
-  its mobile and web donation flows with Stripe so it can focus
-  on what really matters: bringing clean water to every person
-  on the planet. Using Stripe, it also built a new monthly
-  giving programme, which makes it even easier for subscribers
-  to donate throughout the year.
-`,
-    ];
     return {
       rawHtml: texts[0],
     };
   },
   props: ['newnumber'],
+  watch: {
+    newnumber: {
+      immediate: true,
+      deep: true,
+      handler(newValue, oldValue) {
+        this.rawHtml = texts[newValue];
+      },
+    },
+  },
   template: `
 <div
   data-id="text"
@@ -44,7 +54,6 @@ export default {
   >
               <div class="p-12 md:pr-24 md:pl-16 md:py-12">
                 <p class="text-gray-600" v-html="rawHtml">
-                  
                 </p>
                 <a
                   class="
